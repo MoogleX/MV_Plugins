4e1226a9b6176db9a89aa5ba71561b75317a97a2
Alex Seishin <badtz_maru_dressphere@yahoo.com>
2015-11-22 18:04:10 +0800

    Update Moogle_X_ElementBooster.js

--- Moogle_X_ElementBooster.js
+++ Moogle_X_ElementBooster.js
@@ -12,8 +12,16 @@ Moogle_X.EleBost = Moogle_X.EleBost || {};
 
 //=============================================================================
 /*:
- * @plugindesc v1.0 Adds new trait that increase elemental damage output.
+ * @plugindesc v1.1 Adds new trait that increase elemental damage output.
  * @author Moogle_X
+ *
+ * @param Combine Multiple Element Boost
+ * @desc Read the help file for more info about this. 1:Yes 0:No
+ * @default 0
+ *
+ * @param Combine Multiple Element Rate
+ * @desc Read the help file for more info about this. 1:Yes 0:No
+ * @default 0
  *
  * @help
  * ============================================================================
@@ -34,15 +42,46 @@ Moogle_X.EleBost = Moogle_X.EleBost || {};
  * <Element Boost 4: 0%>              // Element 4 will always deal 0 damage.
  *
  * What will happen with "Normal Attack" element?
- * If your battler has multiple attack elements (ex: Physical, Fire, and Ice).
- * Physical doesn't have any element boost trait.
- * Fire has 150% total boost rate.
- * Ice has 180% total boost rate.
- * The element with the highest amount of boost rate will be used for damage
- * calculation.
  *
- * In this case, Ice's element boost rate will be used. The total damage will
- * be multiplied by 180%.
+ * For example, enemy has 200% Fire element rate, 150% Earth element rate, and
+ * 50% Ice element rate.
+ * Actor has Fire and Earth attack element traits.
+ * Actor has 300% Fire Boost, 400% Earth Boost, and 500% Thunder Boost traits.
+ * Then, actor use "Attack" skill (which has "Normal Attack" element) on enemy.
+ *
+ * First, the default engine will check actor's attack element list, which are
+ * Fire and Earth.
+ *
+ * Next, the default engine will check the enemy's element rate from Fire and
+ * Earth and return the one with the highest number (in this case 200%).
+ *
+ * Then my plugin will also do check the actor's element boost traits from Fire
+ * and Earth and return the one with the highest number (in this case 400%).
+ *
+ * The total damage will be multiplied by (200% * 400%) = 800% total multiplier.
+ *
+ * ============================================================================
+ * Combining Multiple Element Rate and Boost
+ * ============================================================================
+ * The above example will happen if you set "Combine Multiple Element Boost"
+ * and "Combine Multiple Element Rate" options to 0 (turn off).
+ *
+ * What happen if you turn on "Combine Multiple Element Rate"?
+ * The default engine will combine enemy's both Fire element rate and Earth
+ * element rate. (200% * 150%) = 300%
+ * Total damage will be multiplied by (300% * 400%) = 1200%
+ *
+ * What happen if you turn on "Combine Multiple Element Boost" while keeping
+ * above option off?
+ * My plugin will combine actor's both Fire and Earth Element Boost rate.
+ * (300% * 400%) = 1200%
+ * Total damage will be multiplied by (200% * 1200%) = 2400%
+ *
+ * What will happen if you turn on both options?
+ * The game will take into accounts enemy's combined rate and actor's combined
+ * boost from Fire and Earth.
+ * The total damage will be multiplied by ((200% * 150%) * (300% * 400%))
+ * = 3600% !!!
  *
  * ============================================================================
  * Terms of Use
@@ -50,16 +89,35 @@ Moogle_X.EleBost = Moogle_X.EleBost || {};
  * Free to use in both commercial and non-commercial project as long as credit
  * is given.
  *
+ * ============================================================================
+ * Change log
+ * ============================================================================
+ * Version 1.1:
+ * - Added "Combine Multiple Element Boost" and "Combine Multiple Element Rate"
+ *   options.
+ *
+ * Version 1.0:
+ * - Completed plugin.
+ *
  */
 //=============================================================================
 
 //=============================================================================
 // Constant Declaration
 //=============================================================================
-Game_BattlerBase.TRAIT_ELEMENT_BOOST   = 111; // Brand new trait's "code" for
-																					  	// element boost effect.
+Game_BattlerBase.TRAIT_ELEMENT_BOOST   = 111; // New trait's "code".
 
 (function() { // IIFE
+
+//=============================================================================
+// Parameter Variables
+//=============================================================================
+
+Moogle_X.EleBost.parameters = PluginManager.parameters('Moogle_X_ElementBooster');
+Moogle_X.EleBost.combineBoost =
+    Number(Moogle_X.EleBost.parameters['Combine Multiple Element Boost']) != 0;
+Moogle_X.EleBost.combineRate =
+    Number(Moogle_X.EleBost.parameters['Combine Multiple Element Rate']) != 0;
 
 //=============================================================================
 // DataManager
@@ -128,11 +186,40 @@ Game_Action.prototype.calcElementBoost = function(subject) {
 
 Game_Action.prototype.elementsMaxBoostRate = function(subject, elements) {
     if (elements.length > 0) {
-        return Math.max.apply(null, elements.map(function(elementId) {
-            return subject.elementBoost(elementId);
-        }, this));
+        if (Moogle_X.EleBost.combineBoost) {
+            var boostList = elements.map(function(elementId) {
+                return subject.elementBoost(elementId);
+            });
+            return boostList.reduce(function(r, value) {
+                return r * value;
+            }, 1);
+        } else {
+            return Math.max.apply(null, elements.map(function(elementId) {
+                return subject.elementBoost(elementId);
+            }, this));
+        }
+
     } else {
         return 1;
+    }
+};
+
+Moogle_X.EleBost.Game_Action_elementsMaxRate =
+    Game_Action.prototype.elementsMaxRate;
+Game_Action.prototype.elementsMaxRate = function(target, elements) {
+    if (Moogle_X.EleBost.combineRate) {
+        if (elements.length > 0) {
+            var rateList = elements.map(function(elementId) {
+                return target.elementRate(elementId);
+            });
+            return rateList.reduce(function(r, value) {
+                return r * value;
+            }, 1);
+        } else {
+            return 1;
+        }
+    } else {
+        return Moogle_X.EleBost.Game_Action_elementsMaxRate.call(this, target, elements);
     }
 };
 
