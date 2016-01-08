eddab652d2dc9958fb30391395fd15b3ff9dc029
Alex Seishin <badtz_maru_dressphere@yahoo.com>
2016-01-08 21:41:45 +0800

    Update Moogle_X_PassiveSkill.js

--- Moogle_X_PassiveSkill.js
+++ Moogle_X_PassiveSkill.js
@@ -12,7 +12,7 @@ Moogle_X.PsvSkl = Moogle_X.PsvSkl || {};
 
 //=============================================================================
 /*:
- * @plugindesc v1.12 Adds passive skills functionality to actors.
+ * @plugindesc v1.13 Adds passive skills functionality to actors.
  * @author Moogle_X
  *
  * @param Default Hide in Battle
@@ -101,6 +101,8 @@ Moogle_X.PsvSkl = Moogle_X.PsvSkl || {};
  * ============================================================================
  * If you use Moogle_X_EquipSkillSystem plugin, position this plugin below it.
  * If you use YEP_ClassChangeCore, position this plugin below it.
+ * If you use YEP_X_SkillCooldowns, position this plugin below it.
+ * If you use Moogle_X_EquipmentLearning, position this plugin above it.
  *
  * ============================================================================
  * Terms of Use
@@ -111,6 +113,9 @@ Moogle_X.PsvSkl = Moogle_X.PsvSkl || {};
  * ============================================================================
  * Change Log
  * ============================================================================
+ * Version 1.13:
+ * - Added compatibility patch with YEP_X_SkillCooldowns v1.07.
+ *
  * Version 1.12:
  * - Added compatibility patch with YEP_ClassChangeCore v1.02.
  *
@@ -348,6 +353,404 @@ Game_BattlerBase.prototype.addedSkillTypes = function() {
 
 
 })(); // IIFE
+
+// Compatibility with Yanfly's Skill Cooldowns.
+if (Imported.YEP_X_SkillCooldowns) {
+
+//=============================================================================
+// Constant Declaration
+//=============================================================================
+
+Game_BattlerBase.TRAIT_PSV_COOLDOWN_DURATION = 115; // New trait code.
+Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_DURATION = 116; // New trait code.
+Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_DURATION = 117; // New trait code.
+Game_BattlerBase.TRAIT_PSV_COOLDOWN_RATE = 118; // New trait code.
+Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_RATE = 119; // New trait code.
+Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_RATE = 120; // New trait code.
+Game_BattlerBase.TRAIT_PSV_COOLDOWN_CHANGE = 121; // New trait code.
+Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_CHANGE = 122; // New trait code.
+Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_CHANGE = 123; // New trait code.
+Game_BattlerBase.TRAIT_PSV_WARMUP_CHANGE = 124; // New trait code.
+Game_BattlerBase.TRAIT_PSV_STYPE_WARMUP_CHANGE = 125; // New trait code.
+Game_BattlerBase.TRAIT_PSV_GLOBAL_WARMUP_CHANGE = 126; // New trait code.
+
+//=============================================================================
+// DataManager
+//=============================================================================
+
+DataManager.processSCDNotetags2 = function(group) {
+	var note1 = /<(?:SKILL)[ ](\d+)[ ](?:COOLDOWN):[ ]([\+\-]\d+)>/i;
+  var note1a = /<(?:SKILL)[ ](.*)[ ](?:COOLDOWN):[ ]([\+\-]\d+)>/i;
+	var note2 = /<(?:STYPE)[ ](\d+)[ ](?:COOLDOWN):[ ]([\+\-]\d+)>/i;
+	var note3 = /<(?:GLOBAL COOLDOWN):[ ]([\+\-]\d+)>/i;
+	var note4 = /<(?:SKILL)[ ](\d+)[ ](?:WARMUP):[ ]([\+\-]\d+)>/i;
+  var note4a = /<(?:SKILL)[ ](.*)[ ](?:WARMUP):[ ]([\+\-]\d+)>/i;
+	var note5 = /<(?:STYPE)[ ](\d+)[ ](?:WARMUP):[ ]([\+\-]\d+)>/i;
+	var note6 = /<(?:GLOBAL WARMUP):[ ]([\+\-]\d+)>/i;
+	for (var n = 1; n < group.length; n++) {
+		var obj = group[n];
+		var notedata = obj.note.split(/[\r\n]+/);
+
+    obj.cooldownChange = {};
+		obj.stypeCooldownChange = {};
+		obj.globalCooldownChange = 0;
+		obj.warmupChange = {};
+		obj.stypeWarmupChange = {};
+		obj.globalWarmupChange = 0;
+
+		for (var i = 0; i < notedata.length; i++) {
+			var line = notedata[i];
+			if (line.match(note1)) {
+				obj.cooldownChange[parseInt(RegExp.$1)] = parseInt(RegExp.$2);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_COOLDOWN_CHANGE;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseInt(RegExp.$2);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note1a)) {
+        var name = String(RegExp.$1).toUpperCase();
+        if (Yanfly.SkillIdRef[name]) {
+          var id = Yanfly.SkillIdRef[name];
+        } else {
+          continue;
+        }
+        obj.cooldownChange[id] = parseInt(RegExp.$2);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_COOLDOWN_CHANGE;
+          var dataId = id;
+          var value = parseInt(RegExp.$2);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+      } else if (line.match(note2)) {
+				obj.stypeCooldownChange[parseInt(RegExp.$1)] = parseInt(RegExp.$2);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_CHANGE;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseInt(RegExp.$2);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note3)) {
+				obj.globalCooldownChange = parseInt(RegExp.$1);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_CHANGE;
+          var dataId = 0;
+          var value = parseInt(RegExp.$1);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note4)) {
+				obj.warmupChange[parseInt(RegExp.$1)] = parseInt(RegExp.$2);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_WARMUP_CHANGE;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseInt(RegExp.$2);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note4a)) {
+        var name = String(RegExp.$1).toUpperCase();
+        if (Yanfly.SkillIdRef[name]) {
+          var id = Yanfly.SkillIdRef[name];
+        } else {
+          continue;
+        }
+        obj.warmupChange[id] = parseInt(RegExp.$2);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_WARMUP_CHANGE;
+          var dataId = id;
+          var value = parseInt(RegExp.$2);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+      } else if (line.match(note5)) {
+				obj.stypeWarmupChange[parseInt(RegExp.$1)] = parseInt(RegExp.$2);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_STYPE_WARMUP_CHANGE;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseInt(RegExp.$2);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note6)) {
+				obj.globalWarmupChange = parseInt(RegExp.$1);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_GLOBAL_WARMUP_CHANGE;
+          var dataId = 0;
+          var value = parseInt(RegExp.$1);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			}
+		}
+	}
+};
+
+DataManager.processSCDNotetags3 = function(group) {
+	var note1 = /<(?:SKILL)[ ](\d+)[ ](?:COOLDOWN DURATION):[ ](\d+)([%％])>/i;
+  var note1a = /<(?:SKILL)[ ](.*)[ ](?:COOLDOWN DURATION):[ ](\d+)([%％])>/i;
+	var note2 = /<(?:STYPE)[ ](\d+)[ ](?:COOLDOWN DURATION):[ ](\d+)([%％])>/i;
+	var note3 = /<(?:GLOBAL COOLDOWN DURATION):[ ](\d+)([%％])>/i;
+	var note4 = /<(?:SKILL)[ ](\d+)[ ](?:COOLDOWN RATE):[ ](\d+)([%％])>/i;
+  var note4a = /<(?:SKILL)[ ](.*)[ ](?:COOLDOWN RATE):[ ](\d+)([%％])>/i;
+	var note5 = /<(?:STYPE)[ ](\d+)[ ](?:COOLDOWN RATE):[ ](\d+)([%％])>/i;
+	var note6 = /<(?:GLOBAL COOLDOWN RATE):[ ](\d+)([%％])>/i;
+	for (var n = 1; n < group.length; n++) {
+		var obj = group[n];
+		var notedata = obj.note.split(/[\r\n]+/);
+
+    obj.cooldownDuration = {};
+		obj.stypeCooldownDuration = {};
+		obj.globalCooldownDuration = 1.0;
+		obj.cooldownRate = {};
+		obj.stypeCooldownRate = {};
+		obj.globalCooldownRate = 1.0;
+
+		for (var i = 0; i < notedata.length; i++) {
+			var line = notedata[i];
+			if (line.match(note1)) {
+				obj.cooldownDuration[parseInt(RegExp.$1)] =
+          parseFloat(RegExp.$2) * 0.01;
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_COOLDOWN_DURATION;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseFloat(RegExp.$2) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note1a)) {
+        var name = String(RegExp.$1).toUpperCase();
+        if (Yanfly.SkillIdRef[name]) {
+          var id = Yanfly.SkillIdRef[name];
+        } else {
+          continue;
+        }
+        obj.cooldownDuration[id] = parseFloat(RegExp.$2) * 0.01;
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_COOLDOWN_DURATION;
+          var dataId = id;
+          var value = parseFloat(RegExp.$2) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+      } else if (line.match(note2)) {
+				obj.stypeCooldownDuration[parseInt(RegExp.$1)] =
+          parseFloat(RegExp.$2) * 0.01;
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_DURATION;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseFloat(RegExp.$2) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note3)) {
+				obj.globalCooldownDuration = parseFloat(RegExp.$1 * 0.01);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_DURATION;
+          var dataId = 0;
+          var value = parseFloat(RegExp.$1) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note4)) {
+				obj.cooldownRate[parseInt(RegExp.$1)] = parseFloat(RegExp.$2) * 0.01;
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_COOLDOWN_RATE;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseFloat(RegExp.$2) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note4a)) {
+        var name = String(RegExp.$1).toUpperCase();
+        if (Yanfly.SkillIdRef[name]) {
+          var id = Yanfly.SkillIdRef[name];
+        } else {
+          continue;
+        }
+        obj.cooldownRate[id] = parseFloat(RegExp.$2) * 0.01;
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_COOLDOWN_RATE;
+          var dataId = id;
+          var value = parseFloat(RegExp.$2) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+      } else if (line.match(note5)) {
+				obj.stypeCooldownRate[parseInt(RegExp.$1)] = parseFloat(RegExp.$2) * 0.01;
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_RATE;
+          var dataId = parseInt(RegExp.$1);
+          var value = parseFloat(RegExp.$2) * 0.01;
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			} else if (line.match(note6)) {
+				obj.globalCooldownRate = parseFloat(RegExp.$1 * 0.01);
+        if (obj.traits) {
+          var code = Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_RATE;
+          var dataId = 0;
+          var value = parseFloat(RegExp.$1 * 0.01);
+          var trait = [{"code":code,"dataId":dataId,"value":value}];
+          obj.traits = obj.traits.concat(trait);
+        }
+
+			}
+		}
+	}
+};
+
+//=============================================================================
+// Game_Battler
+//=============================================================================
+
+Game_Battler.prototype.psvCooldownDuration = function(skillId) {
+    return this.traitsPi(Game_BattlerBase.TRAIT_PSV_COOLDOWN_DURATION, skillId);
+};
+
+Game_Battler.prototype.psvStypeCooldownDuration = function(stypeId) {
+    return this.traitsPi(Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_DURATION, stypeId);
+};
+
+Game_Battler.prototype.psvGlobalCooldownDuration = function() {
+    return this.traitsPi(Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_DURATION, 0);
+};
+
+Game_Battler.prototype.psvCooldownRate = function(skillId) {
+    return this.traitsPi(Game_BattlerBase.TRAIT_PSV_COOLDOWN_RATE, skillId);
+};
+
+Game_Battler.prototype.psvStypeCooldownRate = function(stypeId) {
+    return this.traitsPi(Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_RATE, stypeId);
+};
+
+Game_Battler.prototype.psvGlobalCooldownRate = function() {
+    return this.traitsPi(Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_RATE, 0);
+};
+
+Game_Battler.prototype.psvCooldownChange = function(skillId) {
+    return this.traitsSum(Game_BattlerBase.TRAIT_PSV_COOLDOWN_CHANGE, skillId);
+};
+
+Game_Battler.prototype.psvStypeCooldownChange = function(stypeId) {
+    return this.traitsSum(Game_BattlerBase.TRAIT_PSV_STYPE_COOLDOWN_CHANGE, stypeId);
+};
+
+Game_Battler.prototype.psvGlobalCooldownChange = function() {
+    return this.traitsSum(Game_BattlerBase.TRAIT_PSV_GLOBAL_COOLDOWN_CHANGE, 0);
+};
+
+Game_Battler.prototype.psvWarmupChange = function(skillId) {
+    return this.traitsSum(Game_BattlerBase.TRAIT_PSV_WARMUP_CHANGE, skillId);
+};
+
+Game_Battler.prototype.psvStypeWarmupChange = function(stypeId) {
+    return this.traitsSum(Game_BattlerBase.TRAIT_PSV_STYPE_WARMUP_CHANGE, stypeId);
+};
+
+Game_Battler.prototype.psvGlobalWarmupChange = function() {
+    return this.traitsSum(Game_BattlerBase.TRAIT_PSV_GLOBAL_WARMUP_CHANGE, 0);
+};
+
+Game_Battler.prototype.cooldownDuration = function(skill) {
+		var skillId = skill.id;
+		var stypeId = skill.stypeId;
+		var value = 1.0;
+		value *= this.psvCooldownDuration(skillId);
+		value *= this.psvStypeCooldownDuration(stypeId);
+		value *= this.psvGlobalCooldownDuration();
+		return value;
+};
+
+Game_Battler.prototype.cooldownRate = function(skill) {
+		var skillId = skill.id;
+		var stypeId = skill.stypeId;
+		var value = 1;
+		value *= this.psvCooldownRate(skillId);
+		value *= this.psvStypeCooldownRate(stypeId);
+		value *= this.psvGlobalCooldownRate();
+		return value;
+};
+
+Game_Battler.prototype.flatCooldownChange = function(skill) {
+		var skillId = skill.id;
+		var stypeId = skill.stypeId;
+		var value = 0;
+		value += this.psvCooldownChange(skillId);
+    value += this.psvStypeCooldownChange(stypeId);
+    value += this.psvGlobalCooldownChange();
+    return value;
+};
+
+Game_Battler.prototype.flatWarmupChange = function(skill) {
+		var skillId = skill.id;
+		var stypeId = skill.stypeId;
+		var value = 0;
+		value += this.psvWarmupChange(skillId);
+		value += this.psvStypeWarmupChange(stypeId);
+		value += this.psvGlobalWarmupChange();
+		return value;
+};
+
+//=============================================================================
+// Game_Actor
+//=============================================================================
+
+Game_Actor.prototype.cooldownDuration = function(skill) {
+    return Game_Battler.prototype.cooldownDuration.call(this, skill);
+};
+
+Game_Actor.prototype.cooldownRate = function(skill) {
+		return Game_Battler.prototype.cooldownRate.call(this, skill);
+};
+
+Game_Actor.prototype.flatCooldownChange = function(skill) {
+		return Game_Battler.prototype.flatCooldownChange.call(this, skill);
+};
+
+Game_Actor.prototype.flatWarmupChange = function(skill) {
+		return Game_Battler.prototype.flatWarmupChange.call(this, skill);
+};
+
+//=============================================================================
+// Game_Enemy
+//=============================================================================
+
+Game_Enemy.prototype.cooldownDuration = function(skill) {
+		return Game_Battler.prototype.cooldownDuration.call(this, skill);
+};
+
+Game_Enemy.prototype.cooldownRate = function(skill) {
+		return Game_Battler.prototype.cooldownRate.call(this, skill);
+};
+
+Game_Enemy.prototype.flatCooldownChange = function(skill) {
+		return Game_Battler.prototype.flatCooldownChange.call(this, skill);
+};
+
+Game_Enemy.prototype.flatWarmupChange = function(skill) {
+		return Game_Battler.prototype.flatWarmupChange.call(this, skill);
+};
+
+} // Imported.YEP_X_SkillCooldowns
 
 //=============================================================================
 // End of File
